9a9f2b4f3b6c89c60b5e2bb6b1a93a2c9bcbd457
kinjarapu sravani <51052899+sravs456@users.noreply.github.com>
2021-03-09 21:28:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomDiceIamgeSource1="images/dice"+randomNumber1+".png";
+var randomDiceIamgeSource1="dice"+randomNumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomDiceIamgeSource1);
 
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomDiceImageSource2="images/dice"+randomNumber2+".png";
+var randomDiceImageSource2="dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomDiceImageSource2);
 
 if(randomNumber1>randomNumber2){
